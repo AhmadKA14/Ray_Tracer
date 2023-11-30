608737cb5857f8cd0394cad1c466dbfa8e248a72
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-11-30 18:50:41 -0500

    used color class on main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 #include <fstream>
 
@@ -20,15 +23,8 @@ int main()
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            out_file << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(out_file, pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";
