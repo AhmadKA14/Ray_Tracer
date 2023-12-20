9a2eb07a61a300cd04b187151a4aa189852feae9
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-12-20 08:16:47 +0300

    added lambertian material

--- material.h
+++ material.h
@@ -3,8 +3,7 @@
 
 #include "general.h"
 #include "color.h"
-
-class hit_record;
+#include "hittable.h"
 
 class material
 {
@@ -13,6 +12,24 @@ public:
 
     virtual bool scatter(
         const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const = 0;
+};
+
+class lambertian : public material
+{
+public:
+    lambertian(const color &a) : albedo(a) {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
+        const override
+    {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    color albedo;
 };
 
 #endif
